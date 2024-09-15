69d012e62f0060e9e2cfb85267fd6b86001c6d23
Adrian Escobedo Aguilera <132655170+HadesEA@users.noreply.github.com>
2024-09-14 19:29:03 -0700

    Update Codigo-prueba.cs

--- presentation/csharp/creacionales/abstract_facory/Codigo-prueba.cs
+++ presentation/csharp/creacionales/abstract_facory/Codigo-prueba.cs
@@ -2,12 +2,6 @@ using System;
 
 namespace RefactoringGuru.DesignPatterns.AbstractFactory.Conceptual
 {
-    // The Abstract Factory interface declares a set of methods that return
-    // different abstract products. These products are called a family and are
-    // related by a high-level theme or concept. Products of one family are
-    // usually able to collaborate among themselves. A family of products may
-    // have several variants, but the products of one variant are incompatible
-    // with products of another.
     public interface IAbstractFactory
     {
         IAbstractProductA CreateProductA();
@@ -15,10 +9,6 @@ namespace RefactoringGuru.DesignPatterns.AbstractFactory.Conceptual
         IAbstractProductB CreateProductB();
     }
 
-    // Concrete Factories produce a family of products that belong to a single
-    // variant. The factory guarantees that resulting products are compatible.
-    // Note that signatures of the Concrete Factory's methods return an abstract
-    // product, while inside the method a concrete product is instantiated.
     class ConcreteFactory1 : IAbstractFactory
     {
         public IAbstractProductA CreateProductA()
@@ -32,7 +22,6 @@ namespace RefactoringGuru.DesignPatterns.AbstractFactory.Conceptual
         }
     }
 
-    // Each Concrete Factory has a corresponding product variant.
     class ConcreteFactory2 : IAbstractFactory
     {
         public IAbstractProductA CreateProductA()
@@ -46,14 +35,11 @@ namespace RefactoringGuru.DesignPatterns.AbstractFactory.Conceptual
         }
     }
 
-    // Each distinct product of a product family should have a base interface.
-    // All variants of the product must implement this interface.
     public interface IAbstractProductA
     {
         string UsefulFunctionA();
     }
 
-    // Concrete Products are created by corresponding Concrete Factories.
     class ConcreteProductA1 : IAbstractProductA
     {
         public string UsefulFunctionA()
@@ -70,22 +56,13 @@ namespace RefactoringGuru.DesignPatterns.AbstractFactory.Conceptual
         }
     }
 
-    // Here's the the base interface of another product. All products can
-    // interact with each other, but proper interaction is possible only between
-    // products of the same concrete variant.
     public interface IAbstractProductB
     {
-        // Product B is able to do its own thing...
         string UsefulFunctionB();
 
-        // ...but it also can collaborate with the ProductA.
-        //
-        // The Abstract Factory makes sure that all products it creates are of
-        // the same variant and thus, compatible.
         string AnotherUsefulFunctionB(IAbstractProductA collaborator);
     }
 
-    // Concrete Products are created by corresponding Concrete Factories.
     class ConcreteProductB1 : IAbstractProductB
     {
         public string UsefulFunctionB()
@@ -93,9 +70,6 @@ namespace RefactoringGuru.DesignPatterns.AbstractFactory.Conceptual
             return "The result of the product B1.";
         }
 
-        // The variant, Product B1, is only able to work correctly with the
-        // variant, Product A1. Nevertheless, it accepts any instance of
-        // AbstractProductA as an argument.
         public string AnotherUsefulFunctionB(IAbstractProductA collaborator)
         {
             var result = collaborator.UsefulFunctionA();
@@ -111,9 +85,6 @@ namespace RefactoringGuru.DesignPatterns.AbstractFactory.Conceptual
             return "The result of the product B2.";
         }
 
-       // The variant, Product B2, is only able to work correctly with the
-       // variant, Product A2. Nevertheless, it accepts any instance of
-       // AbstractProductA as an argument.
         public string AnotherUsefulFunctionB(IAbstractProductA collaborator)
         {
             var result = collaborator.UsefulFunctionA();
@@ -122,14 +93,10 @@ namespace RefactoringGuru.DesignPatterns.AbstractFactory.Conceptual
         }
     }
 
-    // The client code works with factories and products only through abstract
-    // types: AbstractFactory and AbstractProduct. This lets you pass any
-    // factory or product subclass to the client code without breaking it.
     class Client
     {
         public void Main()
         {
-            // The client code can work with any concrete factory class.
             Console.WriteLine("Client: Testing client code with the first factory type...");
             ClientMethod(new ConcreteFactory1());
             Console.WriteLine();
