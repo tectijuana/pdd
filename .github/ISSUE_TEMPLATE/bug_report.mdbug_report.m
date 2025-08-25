9e6daeb4f0c83e56fbd97ffd2fc91e12b5f7cb0c
René Solis R. <2523851+IoTeacher@users.noreply.github.com>
2025-08-25 10:53:32 -0700

    Add bug report issue template in Spanish

---
+++ .github/ISSUE_TEMPLATE/bug_report.mdbug_report.m
@@ -0,0 +1,20 @@
+---
+name: Reporte de bug
+about: Ayúdanos a mejorar el curso
+labels: bug
+---
+
+
+**Descripción del bug**
+
+
+**Pasos para reproducir**
+
+
+**Comportamiento esperado**
+
+
+**Capturas/archivos**
+
+
+**Contexto (SO, lenguaje, versión)**
